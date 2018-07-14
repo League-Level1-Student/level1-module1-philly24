116afbe79d1d2b5fc3b28e5cb2277e9442257198
Phillip <Phillip@yens-mbp>
2018-07-14 16:40:17 -0700

    duck only

--- src/Duck.java
+++ src/Duck.java
@@ -1,4 +1,22 @@
 
 public class Duck {
+int numberOfFriends;
+String favoriteFood;
+Duck(String favoriteFood, int numberOfFriends) {
+  	this.favoriteFood = favoriteFood;
+  	this.numberOfFriends = numberOfFriends;
+}	
+
+
+
+public static void main(String[] args) {
+	
+}
+void quack() {
+	
+}
+void waddle() {
+	
+}
 
 }
