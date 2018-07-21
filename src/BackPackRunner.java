64053b06f8419f4c7f0ced7fb8e351bbe4730f98
league <league@wts8.attlocal.net>
2018-07-21 16:03:51 -0700

    NF part2, NF binary

--- src/BackPackRunner.java
+++ src/BackPackRunner.java
@@ -3,5 +3,13 @@
 public class BackPackRunner {
 public static void main(String[] args) {
 	Backpack pack = new Backpack();
+	Textbook book= new Textbook();
+	Pencil pencil= new Pencil();
+	Ruler ruler = new Ruler();
+	
+	pack.putInBackpack(book);
+	pack.putInBackpack(pencil);
+	pack.putInBackpack(ruler);
+	pack.goToSchool();
 }
 }
---
+++ src/extra/personRunner.java
@@ -0,0 +1,10 @@
+package extra;
+
+public class personRunner {
+public static void main(String[] args) {
+	Person myself = new Person();
+	myself.setName("myself");
+	myself.setSuperpower("god mode");
+	Person bryan = 
+}
+}
